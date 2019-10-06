7c7e7440d2ce8614a3440d60ded93e54674baab3
Ben <benlaugherty@gmail.com>
2019-10-06 00:17:21 -0700

    [add] authentication, boostrap routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
+import bcrypt
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,19 +8,59 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/imagestore')
 
 client = MongoClient(host=host)
 db = client.get_default_database()
-users = db.users
 images = db.images
+users = db.users
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', msg='Hello, world!')
+    username = None
+    if 'username' in session:
+        username = session['username']
+
+    return render_template('index.html', username=username)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        login_user = users.find_one({'username': request.form['username']})
+
+        if login_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
+                session['username'] = request.form['username']
+                return redirect(url_for('index'))
+                
+        # TODO: Handle invalid username + password combo
+
+    return render_template('login.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        print(request.form.to_dict())
+        exisiting_user = users.find_one({'username': request.form['username']})
+
+        if exisiting_user is None:
+            hashpass = bcrypt.hashpw(
+                request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert(
+                {'username': request.form['username'], 'email': request.form['email'], 'password': hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+            # TODO: Handle username already taken
+
+        return redirect(url_for('index'))
+
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
-    app.secret_key = SECRET_KEY
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
