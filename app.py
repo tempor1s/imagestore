b7c17a725b93acc6d6a84e63b39c8db700e51520
Ben <benlaugherty@gmail.com>
2019-10-05 18:16:17 -0700

    [add] basic working route and db connection

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/imagestore')
+
+client = MongoClient(host=host)
+db = client.get_default_database()
+users = db.users
+images = db.images
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', msg='Hello, world!')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
