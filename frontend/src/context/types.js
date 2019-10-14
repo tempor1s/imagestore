e03f788a22002767220cc6e10b59329564228360
Ben <benlaugherty@gmail.com>
2019-10-14 15:03:07 -0700

    [add] types and proxy

---
+++ frontend/src/context/types.js
@@ -0,0 +1,14 @@
+// User State
+export const CREATE_USER = 'CREATE_USER';
+export const GET_USER = 'GET_USER';
+export const UPDATE_USER = 'UPDATE_USER';
+export const DELETE_USER = 'DELETE_USER';
+
+// Image State
+export const CREATE_IMAGE = 'CREATE_IMAGE';
+export const GET_IMAGE = 'GET_IMAGE';
+export const UPDATE_IMAGE = 'UPDATE_IMAGE';
+export const DELETE_IMAGE = 'DELETE_IMAGE';
+
+// Loading State
+export const SET_LOADING = 'SET_LOADING';
